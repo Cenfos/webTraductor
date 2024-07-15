0a53d2161a363380a64c3508ca68ce937fdca347
Cenfos <camilo.rguez@gmail.com>
2024-07-15 13:24:02 +0200

    Modo oscuro

--- src/scripts/darkMode.js
+++ src/scripts/darkMode.js
@@ -1,18 +1,17 @@
 // src/scripts/darkMode.js
-const toggleDarkMode = () => {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
-  };
+
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModeButton = document.querySelector('.toggle-dark-mode');
   
-  document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.toggle-dark-mode');
-    if (button) {
-      button.addEventListener('click', toggleDarkMode);
-    }
-  
-    // Cargar la preferencia del modo oscuro
-    if (localStorage.getItem('dark-mode') === 'true') {
-      document.body.classList.add('dark-mode');
-    }
-  });
-  
+  if (darkModeButton) {
+    darkModeButton.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+      localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
+    });
+  }
+
+  // Cargar la preferencia del modo oscuro
+  if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-mode');
+  }
+});
